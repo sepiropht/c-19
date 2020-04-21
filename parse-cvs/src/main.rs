045cfdb3d74689a26ce4c4400e673ce55577a67e
William Mbotta <sergembotta@mailoo.org>
2020-04-21 17:35:25 +0200

    insert data in mongodb

--- parse-cvs/src/main.rs
+++ parse-cvs/src/main.rs
@@ -1,12 +1,11 @@
-use bson::{bson, doc};
-use bson::TimeStamp;
+extern crate chrono;
+use bson::{doc};
 use mongodb::{options::ClientOptions, Client};
 use serde::Deserialize;
 use std::error::Error;
 use std::io;
 use std::process;
-extern crate chrono;
-use chrono::{DateTime, Datelike, NaiveDate, NaiveDateTime, NaiveTime};
+use chrono::{DateTime, NaiveDateTime, NaiveTime, NaiveDate, Utc};
 
 #[derive(Debug, Deserialize)]
 struct Record {
@@ -16,30 +15,28 @@ struct Record {
     code_lieu_naissance: Option<u32>,
     sexe: u32,
     pays_naissance: Option<String>,
-    date_deces: TimeStamp,
+    date_deces: String,
     code_lieu_deces: Option<u32>,
     numero_acte_deces: Option<String>,
 }
 
 fn example() -> Result<(), Box<dyn Error>> {
-    let mut client_options = ClientOptions::parse("mongodb+srv://admin:qUDJuMMCM7opjIQI@cluster0-pap5m.mongodb.net/test?retryWrites=true&w=majority")?;
+    let client_options = ClientOptions::parse("mongodb+srv://admin:qUDJuMMCM7opjIQI@cluster0-pap5m.mongodb.net/test?retryWrites=true&w=majority")?;
     let client = Client::with_options(client_options)?;
     let db = client.database("death_france");
 
     // List the names of the collections in that database.
     let collection = db.collection("person");
 
-    for collection_name in db.list_collection_names(None)? {
-        println!("{}", collection_name);
-    }
     let mut rdr = csv::Reader::from_reader(io::stdin());
     for result in rdr.deserialize() {
-        dbg!("yeah");
         let record: Record = result?;
-        let doc = doc! { "nom": record.nom, "prenom": record.prenom, "sexe": record.sexe, "date_deces": record.date_deces};
+        let date = NaiveDate::parse_from_str(&record.date_deces, "%Y-%m-%d")?;
+        let ms = NaiveTime::from_hms_milli(12, 34, 56, 789);
+        let dt = NaiveDateTime::new(date, ms);
+        let doc = doc! { "nom": record.nom, "prenom": record.prenom, "sexe": record.sexe, "datetime": DateTime::from_utc(dt, Utc)};
 
-        collection.insert_one(doc, None);
-
+        collection.insert_one(doc, None)?;
     }
     Ok(())
 }
